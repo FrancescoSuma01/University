a3f178d9a07fe68c59ba0e33782c74121da78545
FrancescoSuma01 <107925638+FrancescoSuma01@users.noreply.github.com>
2023-10-11 12:45:56 +0200

    Delete Esercizi directory

--- Esercizi/Area_Triangolo/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/Documents/Università/Fondamenti_di_informatica/Esercizi/Area_Triangolo
-# Build directory: C:/Users/franc/Documents/Università/Fondamenti_di_informatica/Esercizi/Area_Triangolo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Esercizi/Equazioni_Secondo_Grado/README.md
+++
@@ -1,7 +0,0 @@
-# Spiegazione della Soluzione
-
-1. bx + c = 0
-
-2. bx = -c
-
-3. x = -c/b
