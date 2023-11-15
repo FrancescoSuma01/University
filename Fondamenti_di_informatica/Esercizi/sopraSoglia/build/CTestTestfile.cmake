29fac8bd13f4869cd12901bfcb16e076d4daf775
FrancescoSuma01 <107925638+FrancescoSuma01@users.noreply.github.com>
2023-11-15 11:52:43 +0100

    Delete Fondamenti_di_informatica/Esercizi/sopraSoglia/build directory

--- Fondamenti_di_informatica/Esercizi/sopraSoglia/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/Documents/Università/Fondamenti_di_informatica/Esercizi/sopraSoglia
-# Build directory: C:/Users/franc/Documents/Università/Fondamenti_di_informatica/Esercizi/sopraSoglia/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
