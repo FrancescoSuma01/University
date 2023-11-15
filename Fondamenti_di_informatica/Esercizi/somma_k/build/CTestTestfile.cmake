04dd430530f8f0f16b0874eaf48b2907dd6f1d89
FrancescoSuma01 <107925638+FrancescoSuma01@users.noreply.github.com>
2023-11-15 11:52:29 +0100

    Delete Fondamenti_di_informatica/Esercizi/somma_k directory

--- Fondamenti_di_informatica/Esercizi/somma_k/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/Documents/Università/Fondamenti_di_informatica/Esercizi/somma_k
-# Build directory: C:/Users/franc/Documents/Università/Fondamenti_di_informatica/Esercizi/somma_k/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
